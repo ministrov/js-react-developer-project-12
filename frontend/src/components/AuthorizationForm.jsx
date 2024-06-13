5a480bcf403907d23c0a9d85244362541df3f831
ministrov <61511030+ministrov@users.noreply.github.com>
2024-06-13 12:04:46 +0300

    fix: fixing eslint errors

--- frontend/src/components/AuthorizationForm.jsx
+++ frontend/src/components/AuthorizationForm.jsx
@@ -1,17 +1,15 @@
 import { Formik, Field, Form } from 'formik';
 
-const AuthorizationForm = () => {
-  return (
-    <Formik
-      initialValues={{ name: '', email: '' }}
-    >
-      <Form>
-        <Field name="name" type="text" />
-        <Field name="password" type="password" />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  )
-};
+const AuthorizationForm = () => (
+  <Formik
+    initialValues={{ name: '', email: '' }}
+  >
+    <Form>
+      <Field name="name" type="text" />
+      <Field name="password" type="password" />
+      <button type="submit">Submit</button>
+    </Form>
+  </Formik>
+);
 
 export default AuthorizationForm;
--- frontend/src/components/Lesson.jsx
+++ frontend/src/components/Lesson.jsx
@@ -1,30 +1,30 @@
-// import { useState } from 'react';
-// import avatar from '../assets/avatar.jpg;'
+import { useState } from 'react';
+import avatar from '../assets/avatar.jpg;'
 
-// const Lesson = () => {
-//   const [isLogoVisible, setIsLogoVisible] = useState(false);
+const Lesson = () => {
+  const [isLogoVisible, setIsLogoVisible] = useState(false);
 
-//   const toggleLogo = () => {
-//     setIsLogoVisible((current) => !current);
-//   };
+  const toggleLogo = () => {
+    setIsLogoVisible((current) => !current);
+  };
 
-//   return (
-//     <div className="wrapper">
-//       <div>
-//         <h2>Do you want to see React Logo?</h2>
-//         <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
-//         Yes
-//         <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
-//         No
-//       </div>
+  return (
+    <div className="wrapper">
+      <div>
+        <h2>Do you want to see React Logo?</h2>
+        <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
+        Yes
+        <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
+        No
+      </div>
 
-//       {isLogoVisible && (
-//         <div>
-//           <img src={avatar} alt="Picture of avatar" />
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
+      {isLogoVisible && (
+        <div>
+          <img src={avatar} alt="Picture of avatar" />
+        </div>
+      )}
+    </div>
+  )
+};
 
-// export default Lesson
+export default Lesson;
